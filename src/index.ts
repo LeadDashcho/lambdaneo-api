f6834f7ca6945e1e851986e8b5e09a4a64cd4eff
Filain <ratecurrent@gmail.com>
2024-12-05 18:51:09 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Application, Response } from 'express';
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware для роботи з JSON
+app.use(express.json());
+
+// Маршрут для перевірки сервера
+app.get('/', (_, res: Response) => {
+    res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:
+    ${PORT}`);
+});
